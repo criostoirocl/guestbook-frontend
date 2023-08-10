a2a95f8b4b626ece636f6184bfb392bbdb01d6bb
Criostóir O'Codlatáin Lachtna <chris@wellola.com>
2023-08-10 13:21:08 +0100

    env

--- config.js
+++ config.js
@@ -2,7 +2,8 @@
 export function getEnv(key) {
     switch(key) {
         case 'VITE_API_URL':
-            return '${VITE_API_URL}';
+            //return '${VITE_API_URL}';
+            return `${VITE_API_URL}`
         default:
             return '';
     }
